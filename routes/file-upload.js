8cf0206cecd872c22e9d5e95a43213ba878be336
alaminhossen899 <alaminhossen899@gmail.com>
2022-10-16 21:36:49 +0600

    multer none and other method use for form data handle and file upload

--- routes/file-upload.js
+++ routes/file-upload.js
@@ -23,18 +23,26 @@ fileUploadRouter.post('/single',upload.single('image1'),(req,res)=>{ /** uploadM
 })
 
 /** multiple file upload using upload.array() */
-fileUploadRouter.post('/multiple',upload.array('image1',3),(req,res)=>{ /** uploadMulter is middleware and mutiple means array file upload second argument 5 means maximum 5 file can upload at a time*/
+fileUploadRouter.post('/multiple',upload.array('image1',5),(req,res)=>{ /** uploadMulter is middleware and mutiple means array file upload second argument 5 means maximum 5 file can upload at a time*/
   console.log(`successfully uploaded multiple file`);
   res.send("successfully uploaded multiple file");
 })
 
 /** multiple file upload using upload.fields() */
-fileUploadRouter.post('/multiple-field',upload.fields([ /** uploadMulter is middleware and upload.fields means multiple field image file */
+/** uploadMulter is middleware and upload.fields means multiple field image file */
+
+fileUploadRouter.post('/multiple-field',upload.fields([
     {name:"image1",maxCount:2},
     {name:"image2",maxCount:2},
 ]),(req,res)=>{ 
-  console.log(`successfully uploaded multiple file`);
-  res.send("successfully uploaded multiple file");
+  console.log(`successfully uploaded multiple file by multiple field`);
+  res.send("successfully uploaded multiple file  by multiple field");
+});
+
+/** if we want only form data then we can use upload.none() */
+fileUploadRouter.post('/none',upload.none(),(req,res)=>{ /** uploadMulter is middleware and mutiple means array file upload second argument 5 means maximum 5 file can upload at a time*/
+  console.log(`only form data`);
+  res.send(`My name is ${req.body.name} and my email name is ${req.body.email}`);
 })
 
 
